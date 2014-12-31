207f6777a3449381229f3f8629c7c945e788fc36
Fabian Röber <fabianroeber@aol.com>
2014-12-31 01:03:13 +0100

    Mapping jetzt in IMPL Klasse

--- getthepoint/src/de/hdm/getthepoint/server/GetThePointImpl.java
+++ getthepoint/src/de/hdm/getthepoint/server/GetThePointImpl.java
@@ -2,11 +2,11 @@ package de.hdm.getthepoint.server;
 
 import java.util.List;
 
-import org.omg.CORBA.DATA_CONVERSION;
-
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.hdm.getthepoint.server.db.DataAcces;
+import de.hdm.getthepoint.server.db.mapper.FrageMapper;
+import de.hdm.getthepoint.server.db.mapper.KategorieMapper;
 import de.hdm.getthepoint.shared.GetThePoint;
 import de.hdm.getthepoint.shared.bo.FrageBo;
 import de.hdm.getthepoint.shared.bo.KategorieBo;
@@ -15,15 +15,25 @@ import de.hdm.getthepoint.shared.bo.KategorieBo;
 public class GetThePointImpl extends RemoteServiceServlet implements
 		GetThePoint {
 
-	@Override
-	public List<KategorieBo> getAllKategorien() throws IllegalArgumentException {
-		List<KategorieBo> resultList = new DataAcces().getAllKategorie();
-		return resultList;
-	}
-	
-	public List<FrageBo> getFragenByKategorie(int kategorie_id) throws IllegalArgumentException {
-		List<FrageBo> resultList = new DataAcces().getFragenByKategorie(kategorie_id);
-		return resultList;
+	DataAcces dataAcces;
+	KategorieMapper kategorieMapper;
+	FrageMapper frageMapper;
+
+	public GetThePointImpl() {
+		dataAcces = new DataAcces();
+		kategorieMapper = new KategorieMapper();
+		frageMapper = new FrageMapper();
 	}
 
+	@Override
+	public List<KategorieBo> getAllKategorien() throws IllegalArgumentException {
+		return kategorieMapper.getModelsAsList(dataAcces.getAllKategorie());
+
+	}
+
+	public List<FrageBo> getFragenByKategorie(int kategorie_id)
+			throws IllegalArgumentException {
+		return frageMapper.getModelsAsList(dataAcces
+				.getFragenByKategorie(kategorie_id));
+	}
 }
